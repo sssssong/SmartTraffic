caa45323d2c3466279487b6a91e6193815cc23c2
guodai <17092505503@163.com>
2016-06-14 09:31:36 +0800

    完成路况简图，采取黑色订制主题，突出道路显示

---
+++ src/com/lnnu/activity/SimpleMap.java
@@ -0,0 +1,78 @@
+package com.lnnu.activity;
+
+
+import java.io.File;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.UiSettings;
+import com.baidu.mapapi.model.LatLng;
+import com.lnnu.smarttraffic.R;
+
+
+
+/**
+ * @author guodai
+ * 2016年6月12日
+ */
+
+public class SimpleMap extends Activity{
+	
+	private MapView mMapView;
+	private BaiduMap mBaiduMap;
+
+	private float level;
+	private String pathname;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//设置自定义地图样式
+				pathname=getFilesDir().getAbsolutePath()+"/dark";
+				mMapView.setCustomMapStylePath(pathname);
+		setContentView(R.layout.simplemap);
+		
+		mMapView = (MapView) findViewById(R.id.sm_bmapview);
+		
+		
+		//关闭zoom控件,与比例尺控件
+		mMapView.showZoomControls(false);
+		mMapView.showScaleControl(false);
+		
+		mBaiduMap = mMapView.getMap();
+		mBaiduMap.setTrafficEnabled(true);
+		
+		UiSettings uiSettings = mBaiduMap.getUiSettings();
+		//设置所有手势开关关闭
+		uiSettings.setAllGesturesEnabled(false);
+		//打开指南针
+		uiSettings.setCompassEnabled(true);
+		
+		
+		//大连中心点121.593478,38.94871
+		//平移后的点121.615498, 38.925857
+		toAppointedMap(mBaiduMap, 38.925857, 121.615498, 14.2f);
+	}
+
+	public void toAppointedMap(BaiduMap map, double lat, double lon, float zoom) {
+		// 自定义一个经纬度，先纬度，后经度
+		LatLng latLng = new LatLng(lat, lon);
+		// 自定义地图状态
+		MapStatus mapStatus = new MapStatus.Builder().target(latLng).zoom(zoom)
+				.build();
+		MapStatusUpdate u = MapStatusUpdateFactory.newMapStatus(mapStatus);
+		// 更新地图状态，加载位置。
+		map.animateMapStatus(u);
+	}
+	
+	
+	
+	
+}
--- src/com/lnnu/smarttraffic/AppAplication.java
+++ src/com/lnnu/smarttraffic/AppAplication.java
@@ -1,8 +1,13 @@
 package com.lnnu.smarttraffic;
 
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
 import com.baidu.mapapi.SDKInitializer;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.res.AssetManager;
 
 
 
@@ -19,6 +24,29 @@ public class AppAplication extends Application{
 		super.onCreate();
 		// 在使用 SDK 各组间之前初始化 context 信息，传入 ApplicationContext
 				SDKInitializer.initialize(this);
+			try {
+				copyMapStyle();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}	
+	}
+	
+	/**
+	 * @author guodai
+	 * 2016年6月12日
+	 * 将地图样式文件从assets目录拷贝到存储空间
+	 */
+	public void copyMapStyle() throws Exception{
+		AssetManager am=this.getAssets();
+		InputStream is = am.open("dark");
+		FileOutputStream fos = openFileOutput("dark", Context.MODE_PRIVATE);
+		byte [] buffer=new byte[1024];
+		int len=-1;
+		while((len=is.read(buffer))!=-1){
+			fos.write(buffer,0,len);
+		}
+		fos.close();
+		is.close();
 	}
 
 }
